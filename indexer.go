de41b80ce656452e2ff508afbdf67797bb94f994
roylee0704 <roylee0704@gmail.com>
2016-05-18 15:30:12 +0800

    completed scanTerm with punctuations as control-break

--- indexer.go
+++ indexer.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"strings"
+	"unicode/utf8"
 )
 
 // A plain hit consists of a capitalization bit, font size,
@@ -26,27 +27,118 @@ func (tf termVector) String() string {
 	return s
 }
 
-// harvest consumes data from reader (line by line), and
+// harvest consumes data from reader using typical bufio.ScanWords and
 // splits out termVector -- (term - []hits)
 func harvest(r io.Reader) termVector {
 
-	freq := make(termVector)
+	tv := make(termVector)
 
 	scanner := bufio.NewScanner(r)
-	scanner.Split(bufio.ScanWords)
+	scanner.Split(ScanTerms)
 
 	var token string
 	var c = 1
 
 	for scanner.Scan() {
 		token = strings.ToLower(scanner.Text())
-		if _, ok := freq[token]; !ok {
-			//	freq[token] = newHitPack()
-		}
-
-		//freq[token].add(hit{pos: c})
-
+		tv[token] = append(tv[token], hit{wordPos: c})
 		c++
 	}
-	return freq
+	return tv
+}
+
+// isControlBreak checks if a rune is a control breaker.
+// - isSpace
+// - isPunctuation
+func isControlBreak(r rune) bool {
+	return isSpace(r) || isPunctuation(r) //space automatically be control breaker.
+}
+
+func isSignificant(r rune) bool {
+	return false
+}
+
+// ScanTerms is a split function for a Scanner that returns each
+// space-separated word of text, with surrounding space deleted. The definition
+// of space is set by unicode.IsSpace.
+//
+// On top of that, it might also return punctuation-seperated word of text.
+// Definition of punctuation is set by significant & insignificant.
+//
+// cases (simplified): return (advance, token, err)
+// 1. "ABC " - spliter found!: return indexOf(last-term-char), token.
+//
+// if spliter not found:
+// 2a. "ABC" - !atEOF: request for more data.
+// 2b. "ABC" - atEOF: return token, err = finalToken
+//
+// advance: number of characters(if not ascii, + width) that you have visited.
+func ScanTerms(data []byte, atEOF bool) (advance int, token []byte, err error) {
+	// ignore spaces
+	var start int
+	for width := 0; start < len(data); start += width {
+		var r rune
+		r, width = utf8.DecodeRune(data[start:])
+		if !isControlBreak(r) {
+			break
+		}
+	}
+
+	// seek for control-break.
+	for i := start; i < len(data); {
+		r, width := utf8.DecodeRune(data[i:])
+		if isControlBreak(r) {
+			return i + width, data[start:i], nil
+		}
+		i += width
+	}
+
+	// no control-break found, check for EOF.
+	if atEOF && len(data) > start {
+		return len(data), data[start:], nil
+	}
+	return start, nil, nil
+}
+
+// isPunctuation serves as control-break checker, atm while insignificant and
+// significant hasn't been implemented, temporarily, all punctuations are
+// considered control-break.
+func isPunctuation(r rune) bool {
+
+	if r == '\'' || r == '-' {
+		return false
+	}
+	if r >= '!' && r <= '/' ||
+		r >= ':' && r <= '@' ||
+		r >= '[' && r <= '`' ||
+		r >= '{' && r <= '~' {
+		return true
+	}
+
+	return false
+}
+
+// isSpace reports whether the character is a Unicode white space character.
+// We avoid dependency on the unicode package, but check validity of the implementation
+// in the tests.
+func isSpace(r rune) bool {
+	if r <= '\u00FF' {
+		// Obvious ASCII ones: \t through \r plus space. Plus two Latin-1 oddballs.
+		switch r {
+		case ' ', '\t', '\n', '\v', '\f', '\r':
+			return true
+		case '\u0085', '\u00A0':
+			return true
+		}
+		return false
+	}
+	// High-valued ones.
+	if '\u2000' <= r && r <= '\u200a' {
+		return true
+	}
+	switch r {
+	case '\u1680', '\u2028', '\u2029', '\u202f', '\u205f', '\u3000':
+		return true
+	}
+	return false
 }
--- indexer_test.go
+++ indexer_test.go
@@ -3,19 +3,28 @@ package main
 import (
 	"fmt"
 	"os"
-	"strings"
 	"testing"
 )
 
 func TestSplit(t *testing.T) {
 
-	tf := harvest(strings.NewReader("hello hello world"))
-	fmt.Printf("%v\n", tf)
+	//tf := harvest(strings.NewReader("hello hello world"))
+	//	fmt.Printf("%v\n", tf)
 
 	fd, _ := os.Open("fixtures/short.txt")
-	tf = harvest(fd)
+	tf := harvest(fd)
 	fmt.Printf("%v\n", tf)
 
-	//fmt.Printf("%d\n", len(tf))
+}
+
+// TestSkipLeadingSpace tests: given a sentence, trim all spaces,
+func TestSkipLeadingSpace(t *testing.T) {
+	// fmt.Println(ScanTerms([]byte("   A "), false))
+	// fmt.Println(ScanTerms([]byte("   A"), false))
+	// fmt.Println(ScanTerms([]byte("   A"), true))
+
+}
+
+func TestIsPunctuation(t *testing.T) {
 
 }
